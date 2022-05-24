4cb709c0efef9050a5c80cbf86a6429557600d18
spancavil <soancavil@gmail.com>
2022-05-23 21:23:24 -0300

    Desarrollo clase 11 II

--- Clase 11 - Tab Navigation/Navigation/Tabs/UserLogged/index.js
+++ Clase 11 - Tab Navigation/Navigation/Tabs/UserLogged/index.js
@@ -1,21 +1,38 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { colors } from '../../../Styles/colors'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import ShopNavigator from '../../Stacks/Shop'
-import CartStack from '../../Stacks/Cart'
+import CartStack from '../../Stacks/Cart';
+import { Entypo } from '@expo/vector-icons';
 
 const BottomTabs = createBottomTabNavigator()
 
 const TabNavigatorLogged = () => {
   return (
-    <BottomTabs.Navigator>
-      <BottomTabs.Screen 
-        name = "ShopTab"
+    <BottomTabs.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: styles.tabBar
+      }}
+    >
+      <BottomTabs.Screen
+        name="ShopTab"
         component={ShopNavigator}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View style={styles.item}>
+                <Entypo name="shop" size={24} color="black" />
+                <Text>Shop</Text>
+              </View>
+            )
+          }
+        }}
       />
       <BottomTabs.Screen
-        name = "CartTab"
+        name="CartTab"
         component={CartStack}
       />
     </BottomTabs.Navigator>
@@ -25,22 +42,22 @@ const TabNavigatorLogged = () => {
 export default TabNavigatorLogged
 
 const styles = StyleSheet.create({
-    tabBar: {
-        shadowColor: colors.shadowTab,
-        shadowOffset: {width: 0, height: 10},
-        shadowOpacity: 0.25,
-        shadowRadius: 0.25,
-        elevation: 5,
-        position: "absolute",
-        bottom: 25,
-        left: 20,
-        right: 20,
-        borderRadius: 15,
-        height: 90,
-    },
-    item: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
+  tabBar: {
+    shadowColor: colors.shadowTab,
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.25,
+    shadowRadius: 0.25,
+    elevation: 5,
+    position: "absolute",
+    bottom: 25,
+    left: 20,
+    right: 20,
+    borderRadius: 15,
+    height: 90,
+  },
+  item: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 })
